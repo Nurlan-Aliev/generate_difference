fcfbcf553dab711101471c7d6281b3c2d28be921
Aliev Nurlan <nualiev420@gmail.com>
2023-01-02 15:52:51 +0400

    refactor generate

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -1,27 +1,77 @@
 from gendiff.opening import open_file
+import itertools
+
+
+ADD = '  + '
+RM = '  - '
+NOT_CHANGE = '    '
+LVL = 1
+DEPTH_LINE = '    '
 
 
 def generate_diff(first_file, second_file):
     open_first = open_file(first_file)
     open_second = open_file(second_file)
-    list_keys = list(open_first.keys() | open_second.keys())
+    return stylish(difference(open_first, open_second))
+
+
+def value_to_str(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return 'null'
+    return value
+
+
+def check(value, value2=None):
+    if value2 is None:
+        value2 = value
+    if isinstance(value, dict):
+        return difference(value, value2)
+    else:
+        return value_to_str(value)
+
+
+def difference(dict_1, dict_2) -> list:
+
+    list_keys = list(dict_1.keys() | dict_2.keys())
     list_keys.sort()
-    result = '{\n'
+    result = []
     for key in list_keys:
-        if key not in open_first.keys():
-            result += f'  + {key}: {bool_to_str(open_second[key])}\n'
-        elif key not in open_second:
-            result += f'  - {key}: {bool_to_str(open_first[key])}\n'
-        elif open_first[key] != open_second[key]:
-            result += f'  - {key}: {bool_to_str(open_first[key])}\n' \
-                      f'  + {key}: {bool_to_str(open_second[key])}\n'
+        value_1 = dict_1.get(key)
+        value_2 = dict_2.get(key)
+
+        if key not in dict_1:
+            result.append((key, ADD, check(value_2)))
+
+        elif key not in dict_2:
+            result.append((key, RM, check(value_1)))
+
         else:
-            result += f'    {key}: {bool_to_str(open_first[key])}\n'
-    result += '}'
+            if value_1 == value_2 or \
+                    isinstance(value_1, dict) and isinstance(value_2, dict):
+                result.append((key, NOT_CHANGE, check(value_1, value_2)))
+
+            else:
+                result.append((key, RM, check(value_1)))
+                result.append((key, ADD, check(value_2)))
+
     return result
 
 
-def bool_to_str(value) -> str:
-    if isinstance(value, bool):
-        return str(value).lower()
-    return value
+def stylish(tree) -> str:
+    def inner_(node, depth):
+        deep_indent_size = DEPTH_LINE * depth
+        lists = []
+        for index in node:
+            name, status, value = index
+            if isinstance(value, list):
+                lists.append(f'{deep_indent_size}{status}{name}:'
+                             f' {inner_(value, depth + LVL)}')
+            else:
+                lists.append(f'{deep_indent_size}{status}{name}:'
+                             f' {value_to_str(value)}')
+        result = itertools.chain('{', lists, [deep_indent_size + '}'])
+        return '\n'.join(result)
+
+    return inner_(tree, 0)
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,12 +1,11 @@
 from gendiff.generate import generate_diff
 
 
-def test_generate_diff():
-    first_json = 'tests/fixtures/file1.json'
-    second_json = 'tests/fixtures/file2.json'
-    first_yaml = 'tests/fixtures/first_file.yaml'
-    second_yaml = 'tests/fixtures/second_file.yaml'
-    result = '''{
+first_json = 'tests/fixtures/file1.json'
+second_json = 'tests/fixtures/file2.json'
+first_yaml = 'tests/fixtures/first_file.yaml'
+second_yaml = 'tests/fixtures/second_file.yaml'
+result = '''{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -15,6 +14,57 @@ def test_generate_diff():
   + verbose: true
 }'''
 
+first_tree_json = 'tests/fixtures/filepath1.json'
+second_tree_json = 'tests/fixtures/filepath2.json'
+first_tree_yaml = 'tests/fixtures/yaml_file1.yaml'
+second_tree_yaml = 'tests/fixtures/yaml_file2.yaml'
+result_tree = '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}'''
+
+
+def test_generate_diff():
     assert generate_diff(first_json, second_json) == result
     assert generate_diff(first_yaml, second_yaml) == result
     assert generate_diff(first_json, second_yaml) == result
@@ -30,6 +80,9 @@ def test_generate_diff():
   - proxy: 123.234.53.22
   - timeout: 50
 }'''
+    assert generate_diff(first_tree_json, second_tree_json) == result_tree
+    assert generate_diff(first_tree_yaml, second_tree_yaml) == result_tree
+    assert generate_diff(first_tree_json, second_tree_yaml) == result_tree
 
 
 if __name__ == '__main__':
