2cc13942f67024900dfe709c29fcf141df2dbd0d
Aliev Nurlan <nualiev420@gmail.com>
2023-01-08 19:17:48 +0400

    dock

--- tests/test_json.py
+++ tests/test_json.py
@@ -4,10 +4,16 @@ from gendiff.gendiff import generate_diff
 
 first_json = 'tests/fixtures/filepath1.json'
 second_json = 'tests/fixtures/filepath2.json'
+first_yaml = 'tests/fixtures/yaml_file1.yaml'
+second_yaml = 'tests/fixtures/yaml_file2.yaml'
 
 
 def test_json_():
     file_for_test = open('tests/fixtures/file_to_test_json.json', 'r')
 
     assert generate_diff(first_json, second_json, json_) == file_for_test.read()
+    file_for_test.close()
+
+    file_for_test = open('tests/fixtures/file_to_test_json.json', 'r')
+    assert generate_diff(first_yaml, second_yaml, json_) == file_for_test.read()
     file_for_test.close()
