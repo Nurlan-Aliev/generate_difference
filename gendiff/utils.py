739ef334be4d7c8fff7f0e81d5120b61e9e3ca0a
Aliev Nurlan <nualiev420@gmail.com>
2023-01-14 15:31:04 +0400

    check utils

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -4,9 +4,7 @@ import json
 
 def open_file(file1):
     if file1.endswith('.json'):
-        with open(file1, "r") as input_file:
-            return json.load(input_file)
+        return json.load(open(file1))
 
     if file1.endswith('.yaml') or file1.endswith('.yml'):
-        with open(file1, "r") as input_file:
-            return yaml.full_load(input_file)
+        return yaml.safe_load(open(file1))
