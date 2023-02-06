4ae8ee2bb49379f821a49477b151bc5d74148849
Aliev Nurlan <nualiev420@gmail.com>
2023-02-06 13:22:05 +0400

    add cli

---
+++ gendiff/cli.py
@@ -0,0 +1,17 @@
+import argparse
+from gendiff.gendiff import generate_diff
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+
+    parser.add_argument('-f', '--format', metavar='',
+                        choices=['stylish', 'plain', 'json'], default='stylish',
+                        help='output format (default: "stylish")')
+
+    args = parser.parse_args()
+    return generate_diff(args.first_file, args.second_file, args.format)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
-import argparse
-from gendiff.gendiff import generate_diff
+from gendiff.cli import cli
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-
-    parser.add_argument('-f', '--format', metavar='',
-                        choices=['stylish', 'plain', 'json'], default='stylish',
-                        help='output format (default: "stylish")')
-
-    args = parser.parse_args()
-    print(generate_diff(args.first_file, args.second_file, args.format))
+    print(cli())
 
 
 if __name__ == '__main__':
